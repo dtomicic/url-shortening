c14cc610ce3ce4e7bdb71b93f269450490a685fe
Darko <dtomicic192@gmail.com>
2023-01-17 15:33:00 +0100

    shortener section done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
+import "./App.css";
+import Functional from "./components/Functional/Functional";
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
-    <div className="whiteBG">
-      <Navigation />
-      <Header />
-    </div>
+    <>
+      <div className="whiteBG">
+        <Navigation />
+        <Header />
+      </div>
+      <div className="grayBG">
+        <Functional />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Functional/Functional.jsx
@@ -0,0 +1,11 @@
+import Shortener from '../Shortener/Shortener'
+import styles from './Functional.module.css'
+
+const Functional = () => {
+  return (
+    <>
+        <Shortener />
+    </>
+  )
+}
+export default Functional
