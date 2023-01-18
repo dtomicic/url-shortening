49b55ddc02543a6984a9555f0d77eb74d18af6bc
Darko <dtomicic192@gmail.com>
2023-01-18 13:18:34 +0100

    functionality done

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,13 +1,30 @@
 import styles from "./History.module.css";
+import React from "react";
 
-const History = ({ link, shortLink }) => {
-  return <div className={styles.container}>
-    <h3 className={styles.longLink}>{link}</h3>
-    <hr className={styles.line}></hr>
-    <div className={styles.leftSection}>
+const History = ({ link, shortLink, copiedUrl, setCopiedUrl }) => {
+  const copyLink = (x) => {
+    navigator.clipboard.writeText(x);
+    setCopiedUrl(x);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.longLink}>{link}</h3>
+      <hr className={styles.line}></hr>
+      <div className={styles.leftSection}>
         <h3 className={styles.shortLink}>{shortLink}</h3>
-        <button className={styles.button}>Copy</button>
+        <button
+          className={
+            copiedUrl === shortLink
+              ? `${styles.button} ${styles.copiedBtn}`
+              : `${styles.button}`
+          }
+          onClick={() => copyLink(shortLink)}
+        >
+          {copiedUrl === shortLink ? "Copied!" : "Copy"}
+        </button>
+      </div>
     </div>
-  </div>;
+  );
 };
 export default History;
--- src/components/Shortener/Shortener.jsx
+++ src/components/Shortener/Shortener.jsx
@@ -1,16 +1,18 @@
 import styles from "./Shortener.module.css";
 
-const Shortener = () => {
+const Shortener = ({ handleInput, getShortLink, error }) => {
   return (
     <div className={styles.container}>
       <form className={styles.form}>
         <div className={styles.inputSection}>
           <input
-            className={styles.input}
+            className={error ? `${styles.input} ${styles.displayErrorBorder}` : `${styles.input}`}
             placeholder="Shorten a link here"
+            onChange={handleInput}
           ></input>
+          <h3 className={error ? `${styles.error} ${styles.displayError}` : `${styles.error}`}><i>Please add a valid link</i></h3>
         </div>
-        <button className={styles.button}>Shorten It!</button>
+        <button className={styles.button} onClick={getShortLink}>Shorten It!</button>
       </form>
     </div>
   );
